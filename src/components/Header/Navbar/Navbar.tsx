8843842ebef5a77f5c724df0b4c1df3a3540835a
robertoDCJ <robertox70@hotmail.com>
2024-04-21 12:13:22 -0300

    Added Link to Navbar

--- src/components/Header/Navbar/Navbar.tsx
+++ src/components/Header/Navbar/Navbar.tsx
@@ -1,28 +1,24 @@
+import Link from "next/link";
+
+const ListItem = ({ children, href }: { children: string; href: string }) => {
+  return (
+    <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500 rounded-xl py-2 px-4 antialiased">
+      <Link href={href}>{children}</Link>
+    </li>
+  );
+};
+
 export const Navbar = () => {
   return (
     <nav className="w-full">
       <ul className="flex md:flex-row flex-col flex-wrap gap-1 items-center justify-center text-sm font-semibold">
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500 rounded-xl py-2 px-4 antialiased">
-          HOME
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          SOBRE NÓS
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          PUBLICAÇÕES
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          ÁREAS DE ATUAÇÃO
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          EQUIPE
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          PRIVACIDADE
-        </li>
-        <li className="cursor-pointer hover:bg-slate-300/20 transition-all duration-500  rounded-xl py-2 px-4 antialiased">
-          CONTATO
-        </li>
+        <ListItem href="/">HOME</ListItem>
+        <ListItem href="/sobre-nos">SOBRE NÓS</ListItem>
+        <ListItem href="/publicacoes">PUBLICAÇÕES</ListItem>
+        <ListItem href="/areas-de-atuacao">ÁREAS DE ATUAÇÃO</ListItem>
+        <ListItem href="/equipe">EQUIPE</ListItem>
+        <ListItem href="/privacidade">PRIVACIDADE</ListItem>
+        <ListItem href="/contato">CONTATO</ListItem>
       </ul>
     </nav>
   );
