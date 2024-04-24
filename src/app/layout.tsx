2eac78a2e0080ff69f21dbfad97220342617f44c
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2024-04-24 01:20:21 -0300

    adicionando página de contato (front-end)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer, Header, WhatsAppButton } from "@/components";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Contato from "./contato/page";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`bg-black text-white ${inter.className}`}>
         <Header className="sticky top-0 z-50" />
         {children}
+        <Contato/>
         <Footer />
         <WhatsAppButton />
       </body>
