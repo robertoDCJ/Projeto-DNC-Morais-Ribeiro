78bfbfdf23a22935d3bbdd404e697c75b192952a
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2024-04-26 10:56:13 -0300

    ajustando home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,27 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       height: {
         "659": "659px",
-=======
-      animation: {
-        slide: 'slide 5s ease-in-out infinite',
-      },
-      keyframes: {
-        slide: {
-          '0%': { backgroundPosition: '0 0' },
-          '100%': { backgroundPosition: '100% 0' },
-        },
-      },
-      gridTemplateColumns: {
-        "auto-fit": "repeat(auto-fit, minmax(375px, 1fr))",
-        "auto-fit1": "repeat(auto-fit, minmax(308px, 1fr))",
-      },
-      height: {
-        "659": "659px",
-        "620": "620px",
->>>>>>> 2a1aa6f32cc5a4e3b4aeed4ef1af961d21d0105f
       },
       width: {
         "456": "456px",
