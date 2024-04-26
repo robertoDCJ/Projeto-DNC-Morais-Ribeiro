c98de11a906a57754b9955d21deb281df756d4b5
Luan Moraes Alencar <luan.moraees21@gmail.com>
2024-04-26 13:08:40 -0300

    finalizando o middleware

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,5 +1,19 @@
-import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import prisma from 'prisma/prisma-client'
 
-export async function POST(req: NextResponse, res: NextRequest){
+const secret: any = process.env.JWT_SECRET;
+
+export async function POST(req: NextRequest, res: NextResponse) {
+
+  const {email, password} = await req.json();
+
+  if(email != process.env.user_email || password != process.env.user_password){
+    return NextResponse.json({msg: "Senha ou email invalidos"});
+  }
+
+  const token = jwt.sign({email}, secret);
+  
+  return NextResponse.json({token});
+
 }
