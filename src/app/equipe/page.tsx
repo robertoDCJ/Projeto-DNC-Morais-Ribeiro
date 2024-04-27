7e7211f1f2827b405cdd18aa052f07366324422a
Juan Useche <juanuseche47@gmail.com>
2024-04-27 01:43:46 -0300

    Desenvolvendo pagina equipe

--- src/app/equipe/page.tsx
+++ src/app/equipe/page.tsx
@@ -1,7 +1,28 @@
+import { Members } from "@/components/Members/Members";
+
+const data = {
+  name: "DRA. NIKOLLY KAROLINE MORAIS E SILVA ",
+  profession: "Advogada",
+  location: "São Paulo/SP",
+  email: "nikolly2@hotmail.com",
+  linkedin: "https://www.linkedin.com/in/juan-useche-79b395235/",
+  image: "/ImgMembers/Nikolly.svg",
+}
+
+
 export default function Equipe() {
   return (
     <div>
-      <h1>Equipe</h1>
+      <div className="grid place-items-center  bg-cover  bg-top " style={{backgroundImage: "url('/ImgSobreNos/Equipe.svg')", height: "659px"}}>
+        <div className="grid grid-cols-2 place-items-center w-full max-w-screen-xl">
+            <h1 className="font-Alegreya font-bold text-5xl">
+              Equipe
+            </h1>
+        </div>
+      </div>
+      <div className="grid place-items-center" style={{backgroundColor: "white"}}>
+      <Members member={data}/>
+      </div>
     </div>
   );
 }
