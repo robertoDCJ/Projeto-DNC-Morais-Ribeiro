1e0338bda75ad41b92b616be56b424cd79930f34
robertoDCJ <robertox70@hotmail.com>
2024-04-28 18:08:02 -0300

    Created lib/db.ts

---
+++ auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [
+    Credentials({
+      async authorize() {
+        return { id: "1", name: "Fulano de Tal", email: "lala@lala.com" };
+      },
+    }),
+  ],
+});
