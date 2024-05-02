4f22e920d9a18f4a7e3d80f4f530c53dcf143f42
robertoDCJ <robertox70@hotmail.com>
2024-05-02 20:42:35 -0300

    Proteged novo-artigo

--- src/app/publicacoes/page.tsx
+++ src/app/publicacoes/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/../auth";
 import Pagination from "@/components/Pagination/Pagination";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Publicacoes() {
   const session = await auth();
@@ -19,11 +20,6 @@ export default async function Publicacoes() {
               backgroundPosition: "center",
             }}
           >
-            {session && (
-              <h1 className="text-9xl text-black font-bold absolute">
-                Hello World
-              </h1>
-            )}
             <div className="flex md:justify-start justify-center items-center md:w-456 w-full">
               <div className="flex flex-col md:w-456  w-full md:px-8 px-4">
                 <h1 className="font-Lato md:text-3xl text-xl ml-12 font-bold ">
@@ -37,10 +33,18 @@ export default async function Publicacoes() {
 
         <div className="flex justify-end items-end mt-4"></div>
 
-        <div className="flex flex-col justify-start items-start p-4">
+        <div className="flex justify-between items-start p-4">
           <h1 className="font-Lato text-3xl text-black font-thin ml-8">
             Artigos
           </h1>
+          {session && (
+            <Link
+              href={"/novo-artigo"}
+              className="font-Lato text-xl text-black font-thin bg-gray-200 hover:bg-gray-300 py-2 px-3 rounded-lg border border-black outline-2 mr-8"
+            >
+              Publicar Novo Artigo
+            </Link>
+          )}
         </div>
 
         <div className="flex flex-col justify-center items-center p-4">
