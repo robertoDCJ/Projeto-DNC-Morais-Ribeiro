dd23be22c937a7d25b32fcd5bf917d40a9aaed0d
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2024-05-09 19:17:37 -0300

    alterando política de privacidade

--- src/app/privacidade/page.tsx
+++ src/app/privacidade/page.tsx
@@ -31,7 +31,6 @@ export default function Privacidade() {
         </div>
         <div className="max-w-3xl mx-auto py-8 px-4">
           <p className="text-xl text-gray-800 leading-relaxed">
-<<<<<<< HEAD
               1.Na Morais & Ribeiro Advocacia e Consultoria Jurídica, 
               levamos a privacidade e a segurança dos dados de nossos clientes muito a sério. 
               Esta Política de Privacidade descreve como coletamos, usamos e protegemos suas informações pessoais.
@@ -63,65 +62,6 @@ export default function Privacidade() {
               7. Podemos atualizar esta Política de Privacidade de tempos em tempos. 
               Recomendamos que você revise esta política regularmente para se manter 
               informado sobre como estamos protegendo suas informações.
-=======
-            1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-            rutrum purus vel neque tempus viverra. Nulla enim nibh, sollicitudin
-            fringilla euismod eget, placerat auctor est. Pellentesque habitant
-            morbi tristique senectus et netus et malesuada fames ac turpis
-            egestas. Nam semper sed eros eget rutrum. Nam vestibulum sem sed
-            fringilla luctus. Curabitur a erat a sapien interdum pellentesque.
-            Integer eu consequat neque, vitae efficitur dui. Vestibulum
-            vestibulum metus nunc, sed imperdiet libero commodo eget. Mauris
-            luctus felis sit amet orci congue aliquet. Maecenas dignissim semper
-            ante at consectetur. Mauris nec sollicitudin elit.
-          </p>
-          <p className="mt-4 text-xl text-gray-800 leading-relaxed">
-            2. Vivamus at lorem scelerisque, suscipit tellus et, interdum odio.
-            Sed congue ante quis molestie sodales. Quisque sed aliquet turpis,
-            sit amet rhoncus urna. Aliquam at dignissim leo, in luctus augue.
-            Maecenas ut turpis elit. Ut vitae sapien euismod, pulvinar ex a,
-            pulvinar massa. Maecenas vitae iaculis felis, sodales scelerisque
-            ante. Maecenas accumsan at turpis non cursus.
-          </p>
-          <p className="mt-4 text-xl text-gray-800 leading-relaxed">
-            3. Pellentesque ac tristique diam. Proin mollis malesuada nibh nec
-            ullamcorper. Suspendisse imperdiet mi eu arcu vulputate, at ornare
-            mi congue. Pellentesque feugiat ac lectus in mattis. Nulla id arcu
-            ullamcorper, volutpat elit maximus, ullamcorper urna.
-          </p>
-          <p className="mt-4 text-xl text-gray-800 leading-relaxed">
-            4. Nam vel mi semper, facilisis sem non, consequat massa. Nam
-            euismod orci in ultrices auctor. Fusce in dapibus felis, a volutpat
-            enim. Suspendisse hendrerit, augue quis ullamcorper ullamcorper,
-            mauris quam tincidunt urna, eu blandit lacus ligula consequat
-            lectus. Maecenas nulla ligula, varius non condimentum vitae, mollis
-            id erat.
-          </p>
-          <p className="mt-4 text-xl text-gray-800 leading-relaxed">
-            5. Donec tempus nisi nunc, tempus ornare urna finibus sit amet.
-            Vestibulum at leo bibendum, vulputate leo consequat, fringilla
-            ligula. Sed id dolor id massa congue pellentesque. Quisque tempor
-            velit eu consequat cursus. Maecenas imperdiet erat ut vestibulum
-            ullamcorper. Vivamus sed turpis diam.
-          </p>
-          <p className="mt-4 text-xl text-gray-800 leading-relaxed">
-            6. Duis eu urna ut turpis euismod finibus. Curabitur vitae augue
-            commodo elit lacinia volutpat sit amet non turpis. Curabitur non
-            faucibus augue. Sed porttitor sollicitudin dui, non pulvinar arcu.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Sed rutrum lectus non ipsum vehicula
-            sodales. Sed nulla arcu, cursus id eleifend vitae, vestibulum non
-            dui. Curabitur pretium dolor in aliquet dapibus.
-          </p>
-          <div className="mt-8">
-            <p className="text-xl text-gray-800 leading-relaxed">
-              7. Vestibulum auctor dui ac vulputate lobortis. Nullam a rutrum
-              orci. Interdum et malesuada fames ac ante ipsum primis in
-              faucibus. Vivamus egestas libero sit amet condimentum tincidunt.
-              Ut dui lorem, congue in leo eu, dapibus venenatis mi. Nunc nec
-              tincidunt ante. Quisque sagittis ullamcorper mauris quis
-              dignissim.
->>>>>>> ba2e4940b33ffe3c373981ddff0ad9705c9ff629
             </p>
           </div>
         </div>
