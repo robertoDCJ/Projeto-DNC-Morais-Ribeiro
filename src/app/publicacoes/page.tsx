77fdae85acc615f87e729b83a979c907b1e0bd1f
Juan Useche <juanuseche47@gmail.com>
2024-05-09 16:45:03 -0300

    ajustando responsivo da página publicações

--- src/app/publicacoes/page.tsx
+++ src/app/publicacoes/page.tsx
@@ -8,10 +8,10 @@ export default async function Publicacoes() {
 
   return (
     <>
-      <main className="bg-gray-200">
-        <div className="flex flex-col justify-center">
+      <main className="bg-gray-200 w-full">
+        <div className="flex flex-col justify-center ">
           <div
-            className={"flex justify-start"}
+            className={"flex justify-center"}
             style={{
               backgroundImage: "url('/ImgPublicacoes/publicacoes.svg')",
               height: "359px",
@@ -20,12 +20,12 @@ export default async function Publicacoes() {
               backgroundPosition: "center",
             }}
           >
-            <div className="flex md:justify-start justify-center items-center md:w-456 w-full">
-              <div className="flex flex-col md:w-456  w-full md:px-8 px-4">
-                <h1 className="font-Lato md:text-3xl text-xl ml-12 font-bold ">
+            <div className="grid grid-cols-1 justify-start items-center max-w-screen-xl w-full">
+              <div className="flex flex-col max-w-2xl w-full p-5">
+                <h1 className="font-Lato md:text-3xl text-xl font-bold">
                   PUBLICAÇÕES
                 </h1>
-                <div className="border border-white my-3 ml-12 antialiased" />
+                <div className="border border-white my-3  antialiased" />
               </div>
             </div>
           </div>
@@ -33,18 +33,20 @@ export default async function Publicacoes() {
 
         <div className="flex justify-end items-end mt-4"></div>
 
-        <div className="flex justify-between items-start p-4">
-          <h1 className="font-Lato text-3xl text-black font-thin ml-8">
+        <div className="flex justify-center items-center p-4 w-full">
+          <div className="flex flex-col w-80 gap-2  items-center max-w-screen-xl md:justify-between md:w-full p-8 md:flex md:flex-row">
+          <h1 className="font-Lato text-3xl text-black font-thin">
             Artigos
           </h1>
           {session && (
             <Link
               href={"/novo-artigo"}
-              className="font-Lato text-xl text-black font-thin bg-gray-200 hover:bg-gray-300 py-2 px-3 rounded-lg border border-black outline-2 mr-8"
+              className="font-Lato text-xl text-black font-thin bg-gray-200 hover:bg-gray-300 py-2 px-3 rounded-lg border border-black outline-2"
             >
               Publicar Novo Artigo
             </Link>
           )}
+          </div>
         </div>
 
         <div className="flex flex-col justify-center items-center p-4">
